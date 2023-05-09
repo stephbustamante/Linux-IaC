b508a54fd018e3c58727ee0e156632b989f8d906
stephbustamante <stephbustamante@live.com>
2023-05-09 19:15:12 +0000

    Scritp IaC v.1

---
+++ ambiente_config.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo "Configurando ambiente..."
+
+mkdir /publico /adm /ven /sec
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+chown root:GRP_ADM /adm/
+chown root:GRP_VEN /ven/
+chown root:GRP_SEC /sec/
+
+chmod 777 /publico/
+chmod 770 /adm/
+chmod 770 /ven/
+chmod 770 /sec/
+
+useradd carlos -c "Carlos Antônio" -m -s /bin/bash -p $(openssl passwd -6 123456) -G GRP_ADM
+passwd carlos -e
+
+useradd maria -c "Maria Aparecida" -m -s /bin/bash -p $(openssl passwd -6 123456) -G GRP_ADM
+passwd maria -e
+
+useradd joao -c "João Medeiros" -m -s /bin/bash -p $(openssl passwd -6 123456) -G GRP_ADM
+passwd joao -e
+
+useradd debora -c "Débora Nascimento" -m -s /bin/bash -p $(openssl passwd -6 123456) -G GRP_VEN
+passwd debora -e
+
+useradd sebastiana -c "Sebastiana das Dores" -m -s /bin/bash -p $(openssl passwd -6 123456) -G GRP_VEN
+passwd sebastiana -e
+
+useradd roberto -c "Roberto Lopes" -m -s /bin/bash -p $(openssl passwd -6 123456) -G GRP_VEN
+passwd roberto -e
+
+useradd josefina -c "Josefina Lagos" -m -s /bin/bash -p $(openssl passwd -6 123456) -G GRP_SEC
+passwd josefina -e
+
+useradd amanda -c "Amanda Farias" -m -s /bin/bash -p $(openssl passwd -6 123456) -G GRP_SEC
+passwd amanda -e
+
+useradd rogerio -c "Rogério Dias" -m -s /bin/bash -p $(openssl passwd -6 123456) -G GRP_SEC
+passwd rogerio -e
+
+echo "Ambiente pronto!"
